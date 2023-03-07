59931f76feec5b62af0ca7dd0de1226fb4c7cf95
19ALESSANDROLORENZET <alessandro.lorenzetti@matteidigitale.org>
2023-03-07 13:03:41 +0100

    first commit

---
+++ app/src/main/java/com/example/calcolatrice20/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.calcolatrice20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button somma=(Button) findViewById(R.id.buttonSomma);
+        EditText v1=(EditText) findViewById(R.id.editTextn1);
+        EditText v2=(EditText) findViewById(R.id.editTextn2);
+        TextView tot=(TextView) findViewById(R.id.textViewRisultato);
+
+        somma.setOnClickListener(new View.OnClickListener())
+        {
+
+        }
+
+    }
+}
